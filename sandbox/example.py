b5205809aa50891d233f57be61bf3f3159f9aeec
Jacob Turner <turnerlj@live.unc.edu>
2021-02-06 09:12:38 -0500

    Progress on exercise one

---
+++ sandbox/example.py
@@ -0,0 +1,12 @@
+"""An example, simple program."""
+
+print("Hello, world")
+quiz: int = 100
+print(quiz)
+print("Goodbye.")
+
+a: int = 2021
+b: float = 3.14
+c: str = "hello"
+d: bool = a > 2020 and b < 4.0 or not c != "world"
+print(d)
